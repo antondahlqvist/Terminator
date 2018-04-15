e92ddc2b6fb03004e112d02eab7c5920fb41ea02
Anton <antondahlqvist92@gmail.com>
2018-04-15 13:19:12 +0200

    trororlo

--- Trap20/Trap20.ino
+++ Trap20/Trap20.ino
@@ -1,30 +1,42 @@
 #include <AltSoftSerial.h>
 #include <BareBoneSim800.h>
 
-#include <EEPROM.h>
+#include "tasks.h"
 
-#define SENSOR_PIN 2
+const int sensorpin = 2;            //Koppla magneten mellan pin 2 och jord.
 
+void setup() {
+  Serial.begin(9600);
 
-
-
-void setup_sensor(void){
-  const int sensorpin = SENSOR_PIN;            //Koppla magneten mellan pin 2 och jord.
-  pinMode(sensorpin, INPUT_PULLUP);   //Input, Pullup motstånd
-  attachInterrupt(digitalPinToInterrupt(sensorpin), ISR_Sensor, FALLING);
+  
+  setup_sensor();  //Konfigruerar sensorns inpupinne och interrupt
+  tasks_init();   //Initierar task hanteraren
 }
 
-ISR_Sensor(){
+void setup_sensor(void){
+  
+  pinMode(sensorpin, INPUT_PULLUP);   //Input, Pullup motstånd
+  attachInterrupt(digitalPinToInterrupt(sensorpin), ISR_Sensor, RISING);
+}
+
+void ISR_Sensor(){
+  add_task_to_queue(send_status_text);      //Lägger till skicka sms i kön
+  detachInterrupt(digitalPinToInterrupt(sensorpin));
+}
+
+void send_status_text(void) {
+ Serial.println("Weekly text");
+}
+
+void send_triggered_trap_text(){
+ Serial.println("Weekly text"); 
+}
+
+void go_to_sleep(){
   
 }
 
-void setup() {
-
-  setup_sensor();  //Konfigruerar sensorns inpupinne och interrupt
-
-}
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  run_tasks();
+  go_to_sleep();
 }
