218d0558001a879a0b0f75c82930c6897d1789bc
Anton <antondahlqvist92@gmail.com>
2018-04-15 12:19:39 +0200

    hej

--- Trap20/Trap20.ino
+++ Trap20/Trap20.ino
@@ -1,5 +1,26 @@
+#include <AltSoftSerial.h>
+#include <BareBoneSim800.h>
+
+#include <EEPROM.h>
+
+#define SENSOR_PIN 2
+
+
+
+
+void setup_sensor(void){
+  const int sensorpin = SENSOR_PIN;            //Koppla magneten mellan pin 2 och jord.
+  pinMode(sensorpin, INPUT_PULLUP);   //Input, Pullup motstånd
+  attachInterrupt(digitalPinToInterrupt(sensorpin), ISR_Sensor, FALLING);
+}
+
+ISR_Sensor(){
+  
+}
+
 void setup() {
-  // put your setup code here, to run once:
+
+  setup_sensor();  //Konfigruerar sensorns inpupinne och interrupt
 
 }
 
